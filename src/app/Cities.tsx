c34d741a3bea85880f272199e8c4ca2d7cab1b24
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2019-09-09 17:38:37 -0300

    Adding useObserver.

--- src/app/Cities.tsx
+++ src/app/Cities.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import {useObserver} from 'mobx-react-lite';
 
 import {useMobxStore} from '../store/context';
 
 const Cities: React.FC = () => {
 	const store = useMobxStore();
 
-	return (
+	return useObserver(() => (
 		<ul>
 			{store.allCities.map((city: string) =>
 				<li>{city}</li>
 			)}
 		</ul>
-	);
+	));
 };
 
 export default Cities;
